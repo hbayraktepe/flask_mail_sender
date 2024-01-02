f37ce2760d753aa1f169f9c850a9c49a1395cdfb
hbayraktepe <149676281+hbayraktepe@users.noreply.github.com>
2024-01-02 08:15:29 +0300

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -3,10 +3,23 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 
+global_smtp_connection = None
+def get_smtp_connection():
+    global global_smtp_connection
+    if global_smtp_connection is None or not global_smtp_connection.noop()[0] == 250:
+        smtp_server = os.getenv('SMTP_SERVER')
+        smtp_port = 587
+        smtp_username = os.getenv('SMTP_USERNAME')
+        smtp_password = os.getenv('SMTP_PASSWORD')
 
+        global_smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
+        global_smtp_connection.starttls()
+        global_smtp_connection.ehlo()
+        global_smtp_connection.login(smtp_username, smtp_password)
+    return global_smtp_connection
 def send_email_async(name, email, message):
     try:
-        # E-posta gönderim işlemini burada gerçekleştirin
+        server= get_smtp_connection()
         smtp_server = os.getenv('SMTP_SERVER')
         smtp_port = 587
         smtp_username = os.getenv('SMTP_USERNAME')
@@ -24,14 +37,6 @@ def send_email_async(name, email, message):
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'plain'))
 
-        # Create SMTP connection and send email
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_username, smtp_password)
-
-            server.ehlo()
-
-            # mail gönderme işlemi
-            server.sendmail(smtp_username, to_address, msg.as_string())
+        server.sendmail(smtp_username, to_address, msg.as_string())
     except Exception as e:
         print(f"An exception occurred: {e}")
